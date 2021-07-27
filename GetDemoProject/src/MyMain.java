2a94cf92bc1fcdad1cfcfa743c79885e3a13ac5e
Prashu106 <prashusaxena23@gmail.com>
2021-07-27 17:34:55 +0530

    First Commmit

---
+++ GetDemoProject/src/MyMain.java
@@ -0,0 +1,9 @@
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		Person p =new Person(100,"Ram");
+		System.out.println(p.getIdo());
+	}
+
+}
---
+++ GetDemoProject/src/Person.java
@@ -0,0 +1,22 @@
+
+public class Person {
+	private int ido;
+	private String name;
+	public Person(int ido, String name) {
+		super();
+		this.ido = ido;
+		this.name = name;
+	}
+	public int getIdo() {
+		return ido;
+	}
+	public void setIdo(int ido) {
+		this.ido = ido;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
